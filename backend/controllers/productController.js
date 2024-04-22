f2fdd8684e394173a30e7ff4747077f9e30e70e5
inestaleb <inesines139103@gmail.com>
2024-04-22 14:01:28 +0200

    update and delete product and Creating errors middlewre

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product')
-
+const ErrorHandler=require('../utils/errorHandler')
 // create new Product => /api/v1/product/new
 
 exports.newProduct= async (req,res,next)=>{
@@ -22,17 +22,53 @@ exports.getProducts= async (req,res,next)=>{
     })
 }
 
-// Get Single Product  details => /api/v2/product/:id
+// Get Single Product  details => /api/v1/product/:id
 exports.getSingleProduct= async (req,res,next)=>{
 const product= await Product.findById(req.params.id)
+if(!product){
+   
+    return next(new ErrorHandler('Product not found',404));
+}
+res.status(200).json({
+    success:true,
+    product
+})
+}
+
+//Update Product => /api/v1/product/:id
+
+exports.updateProduct= async(req,res,next)=>{
+    let product= await Product.findById(req.params.id)
 if(!product){
     return res.status(404).json({
         success:false,
         message:'Product not found '
      } )
 }
+product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+    new:true,
+    runValidators:true,
+    useFindAndModify:false
+});
 res.status(200).json({
     success:true,
     product
 })
+}
+
+//Delete Product => api/v1/admin/product/:id
+exports.deleteProduct =async (req,res,next)=>{
+    const product= await Product.findByIdAndDelete(req.params.id);
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message:'Product not found '
+         } )
+    }
+  
+  
+      res.status(200).json({
+        success:true,
+        message:"Product is deleted"
+    })
 }
