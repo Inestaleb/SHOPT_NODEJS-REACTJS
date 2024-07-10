d4d17c8b1a1c6101ebe428898fff2a7a1d771cd4
Inestaleb <inesines139103@gmail.com>
2024-07-10 16:00:47 +0200

    add api featchure and modify productCTROLLER

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -13,9 +13,20 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "PRODUCTION") {
     let error = { ...err };
     error.message = err.message;
+    // wrong Mongoose object id Error
+    if (err.name == "CastError") {
+      const message = `Resource not found. Invalid: ${err.path} `;
+      error = new ErrorHandler(message, 400);
+    }
+    //Handling Mongoose validation error
+    if (err.name === "ValidationError") {
+      const message = object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
-      message: error.message|| 'internal Server Error',
-    })
+      message: error.message || "internal Server Error",
+    });
   }
 };
